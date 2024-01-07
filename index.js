e6e621cad48478696b47cdb5f2c13f44d5d9f060
The Captaan <thecaptaan@proton.me>
2024-01-07 17:10:05 +0530

    Final Update

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
+// document.addEventListener("contextmenu", function (e) {
+//     e.preventDefault();
+// });
+const originalBody = document.body.cloneNode(true);
+
+// function checkConsole() {
+//     if (window.outerWidth - window.innerWidth > 200 || window.outerHeight - window.innerHeight > 200) {
+//         // Console is open, remove body
+//         document.body.remove();
+//     } else {
+//         // Console is closed, restore body
+//         document.documentElement.replaceChild(originalBody, document.body);
+//     }
+// }
+
+// Check console status periodically
+// setInterval(checkConsole, 1000);
 document.addEventListener('DOMContentLoaded', () => {
     let hamburger = document.getElementById("hamburger");
+    let navList = document.querySelector(".nav__list");
     let navContainer = document.querySelector(".nav__links__container")
-
+    let navItem = document.querySelectorAll(".nav__item");
+    hamburger.addEventListener("click", () => {
+        navContainer.classList.toggle("hamburger__position");
+        navList.classList.toggle("hamburger__show");
+        hamburger.src = navList.classList.contains("hamburger__show") ? "./images/cancel.svg" : "./images/hamburger.svg";
+    });
+    navItem.forEach(item => {
+        item.addEventListener("click", () => {
+            navContainer.classList.remove("hamburger__position");
+            navList.classList.remove("hamburger__show");
+            hamburger.src = "./images/hamburger.svg";
+        });
+    });
 });
